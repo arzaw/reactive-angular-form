735bbcf0ad106830ca17c90986fa8ebacd519e78
arzaw <arzaw_11815115@nitkkr.ac.in>
2021-12-23 00:41:41 +0530

    midway

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
-import { Form } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   genders = ['male', 'female'];
-  signupForm: Form                                        //hold the form at the end 
+  signupForm: FormGroup                                        //hold the form at the end 
 
-  
+  ngOnInit(): void {                                      //implemented before the rendering of the componenet so that a form is created
+    this.signupForm = new FormGroup({
+      'userData': new FormGroup({
+        'username': new FormControl(null, Validators.required),
+        'email': new FormControl(null, [Validators.required, Validators.email]),
+      }),
+      'gender': new FormControl('Male')
+    })
+  }
+
+  onSubmit(){
+    console.log(this.signupForm)
+  }
 }
